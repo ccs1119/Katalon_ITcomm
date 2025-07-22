1ae29a47fc635831a0789dcbee12257c43f4ed65
martinwhui <manhuiwong313@gmail.com>
2025-07-22 14:51:07 +0800

    Tyring to commit sys-001-0001

---
+++ Object Repository/Page_Sign In/div_ArcGIS login.rs
@@ -0,0 +1,173 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_ArcGIS login</name>
+   <tag></tag>
+   <elementGuidId>4dfef0c2-9bd2-47ec-acab-4f5e7b795d53</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='loginTitle']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#loginTitle</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=tab[name=&quot;ArcGIS login&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>5a8314bc-407e-4a39-b86a-a2926f4fa698</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>accordion-title</value>
+      <webElementGuid>0afdc7bb-a9fc-46be-892d-9cd3510d2a6c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>loginTitle</value>
+      <webElementGuid>48f0beeb-2c2a-433f-9127-a03c1534a2ad</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>role</name>
+      <type>Main</type>
+      <value>tab</value>
+      <webElementGuid>080ae6c2-81fc-45c0-82f4-f24c2cdbf2e4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tabindex</name>
+      <type>Main</type>
+      <value>0</value>
+      <webElementGuid>a37a1c60-30b4-48f0-95b2-8dd96ea6f243</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-controls</name>
+      <type>Main</type>
+      <value>loginPanel</value>
+      <webElementGuid>359c7c0a-047a-427a-9ab4-85ed32e43bfc</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+            
+                
+                    
+                
+            
+            
+                ArcGIS login
+            
+        </value>
+      <webElementGuid>1a02a5dd-673c-457e-9e0a-d6806357d96a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;loginTitle&quot;)</value>
+      <webElementGuid>e8d46f6d-4214-4b84-83b1-f8926969c549</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//div[@id='loginTitle']</value>
+      <webElementGuid>5679587f-7540-4405-9e5a-8f138d29843e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='loginComponents']/section[2]/div/div/div</value>
+      <webElementGuid>99b5837f-496a-4793-b34c-4e69693de314</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Sign in to HKIA Enterprise GIS (UAT) with'])[1]/following::div[9]</value>
+      <webElementGuid>d68128cb-b137-4cfd-b8ef-15607910c377</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='HKIA Enterprise GIS (UAT)'])[1]/following::div[14]</value>
+      <webElementGuid>c7c9f3bc-7e6c-4319-8add-15d17d18db32</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Keep me signed in'])[1]/preceding::div[3]</value>
+      <webElementGuid>1c4e92e1-2674-4df1-b656-b26aeccabdb1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//section[2]/div/div/div</value>
+      <webElementGuid>f312bc36-c471-46c5-b900-13d831b1f66a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[@id = 'loginTitle' and (text() = '
+            
+                
+                    
+                
+            
+            
+                ArcGIS login
+            
+        ' or . = '
+            
+                
+                    
+                
+            
+            
+                ArcGIS login
+            
+        ')]</value>
+      <webElementGuid>aec35804-0e9f-4d63-8045-49ca7e708846</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
